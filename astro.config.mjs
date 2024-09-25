fab34ba46674be4f1f07de97aa0ffd0a22e8665c
Илья Терёшин <terkin1805@gmail.com>
2024-09-25 16:57:40 +0400

    sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,16 @@
+import sitemap from "@astrojs/sitemap";
+import tailwind from "@astrojs/tailwind";
 // @ts-check
-import { defineConfig } from 'astro/config';
-
-import tailwind from '@astrojs/tailwind';
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind({
-    applyBaseStyles: false,
-  })],
-  trailingSlash:"never",
+	site: "https://astro-webcache.vercel.app/",
+	integrations: [
+		tailwind({
+			applyBaseStyles: false,
+		}),
+		sitemap(),
+	],
+	trailingSlash: "never",
 });
